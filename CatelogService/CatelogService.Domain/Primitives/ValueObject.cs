9ce8f5753c22100f14971c56dde05bcda3afed84
Ravindu <ravindurasanjana1998@gmail.com>
2024-09-24 00:29:29 +0530

    init

---
+++ CatelogService/CatelogService.Domain/Primitives/ValueObject.cs
@@ -0,0 +1,28 @@
+﻿namespace CatelogService.Domain.Primitives;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+
+    public abstract IEnumerable<object> GetAtomicValues();
+
+    public bool Equals(ValueObject? other)
+    {
+       return other is ValueObject && AreValuesEqual(other);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is ValueObject other && AreValuesEqual(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return GetAtomicValues().Aggregate(default(int), HashCode.Combine);
+    }
+
+    private bool AreValuesEqual(ValueObject other)
+    {
+        return GetAtomicValues().SequenceEqual(other.GetAtomicValues());
+    }
+
+}
---
+++ CatelogService/CatelogService.Domain/ValueObjects/Price.cs
@@ -0,0 +1,14 @@
+﻿using CatelogService.Domain.Primitives;
+
+namespace CatelogService.Domain.ValueObjects;
+
+public sealed class Price : ValueObject
+{
+    public decimal Amount { get; }
+    public string Currency { get; } = null!;
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Amount; 
+        yield return Currency;
+    }
+}
